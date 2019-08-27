5ddd81a2b501963b92f2f9a3f1752165756d9157
Robert Leaf <34246014+rtleaf@users.noreply.github.com>
2019-08-27 10:46:30 -0500

    Add files via upload

---
+++ render_loop.R
@@ -0,0 +1,9 @@
+library(rmarkdown)  # for render
+
+html.file.name <- paste0(unlist(strsplit(dir(pattern = ".rmd"),".rmd")),".html")
+
+for (j in 1:length(dir(pattern = ".rmd"))) {
+render(
+  input = dir(pattern = ".rmd")[j],  # path to the template
+  output_file = html.file.name[j])   # folder in which to put the output file
+}
